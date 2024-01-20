91b1c7ce86d79f61cf9450a3981757729171b220
oussoubicodes <oussniak789@gmail.com>
2024-01-20 11:09:09 +0000

    Signup implemented

--- app/src/main/java/com/example/elderhelper/Signup.java
+++ app/src/main/java/com/example/elderhelper/Signup.java
@@ -1,20 +1,25 @@
 package com.example.elderhelper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Signup extends AppCompatActivity {
-    EditText iFirstname, iLastname, iEmail, iNumber /*,iPassword*/;
-    Button iNextButton, iRegisterButton, iLoginButton;
+    EditText iFullname, iEmail, idobQ, iChildrenQ, iSpouseQ;
+    Button iFinishButton, iRegisterButton, iLoginButton;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
     @Override
@@ -23,25 +28,75 @@ public class Signup extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         //Login Details
-        iFirstname = findViewById(R.id.firstname);
-        iLastname = findViewById(R.id.lastname);
+        iFullname = findViewById(R.id.fullname);
         iEmail = findViewById(R.id.email);
-        iNumber = findViewById(R.id.phone);
-
-        //iPassword = findViewById(R.id.)
-
+        idobQ = findViewById(R.id.editTextNumberPassword);
+        iChildrenQ = findViewById(R.id.editTextNumberPassword2);
+        iSpouseQ = findViewById(R.id.editTextTextPassword2);
 
         //Buttons
-        iNextButton = findViewById(R.id.registerButton);
+        iFinishButton = findViewById(R.id.registerButton);
         iRegisterButton = findViewById(R.id.registerButton);
         iLoginButton = findViewById(R.id.loginButton);
 
+        //Other
         fAuth = FirebaseAuth.getInstance(); //gets the current instance of database from firebase
-
         progressBar = findViewById(R.id.progressBar);
 
-        iNextButton.setOnClickListener(new View.OnClickListener(){
+        //Checks if user is already logged in
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        iFinishButton.setOnClickListener(new View.OnClickListener(){
             @Override
+            public void onClick(View v){
+                String email = iEmail.getText().toString().trim(); //Used trim() to format the data
+                String dobQ = idobQ.getText().toString().trim();
+                String childrenQ = iChildrenQ.getText().toString().trim();
+                String spouseQ = iSpouseQ.getText().toString().trim();
+
+                //Combining all the security questions into one password
+                String password = idobQ.getText().toString().trim()+
+                        iChildrenQ.getText().toString().trim()+
+                        iSpouseQ.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    iEmail.setError("An email is required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(dobQ)){
+                    idobQ.setError("All security questions need to be answered");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(childrenQ)){
+                    iChildrenQ.setError("All security questions need to be answered");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(spouseQ)){
+                    iSpouseQ.setError("All security questions need to be answered");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                //Adds user to the database
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Signup.this, "User Created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }else{
+                            Toast.makeText(Signup.this, "Error !" +task.getException().getMessage() ,Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
         });
     }
 }
